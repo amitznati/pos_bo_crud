645491bd3e7477047124d25794063ad78733207c
amitznati <amitznati@gmail.com>
2017-04-13 13:30:32 +0300

    gitignore fix

---
+++ resources/views/vendor/backpack/base/inc/menu.blade.php
@@ -0,0 +1,45 @@
+<div class="navbar-custom-menu pull-{!!$left!!}">
+    <ul class="nav navbar-nav">
+        <!-- =================================================== -->
+        <!-- ========== Top menu items (ordered left) ========== -->
+        <!-- =================================================== -->
+
+        <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
+
+        <!-- ========== End of top menu left items ========== -->
+    </ul>
+</div>
+
+<div class="navbar-custom-menu">
+    <ul class="nav navbar-nav">
+      <!-- ========================================================= -->
+      <!-- ========== Top menu right items (ordered left) ========== -->
+      <!-- ========================================================= -->
+      <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
+        @if (Auth::guest())
+
+            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/login') }}">{{ trans('backpack::base.login') }}</a></li>
+            @if (config('backpack.base.registration_open'))
+            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/register') }}">{{ trans('backpack::base.register') }}</a></li>
+            @endif
+        @else
+            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/logout') }}"><i class="fa fa-btn fa-sign-out"></i> {{ trans('backpack::base.logout') }}</a></li>
+        @endif
+        <li>
+          <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style="margin: 10px;background: transparent; border: none;color: white;">
+              Languages
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            @foreach( array('en','he') as $lang)
+              <li class="{{ session('locale') == $lang ? 'active' : '' }}">
+                <a href="{{ route('locale.setlocale', $lang) }}" >{{ $lang }}</a>
+              </li>
+            @endforeach
+            </ul>
+          </div>
+        </li>
+       <!-- ========== End of top menu right items ========== -->
+    </ul>
+</div>
