12cef8ca558c14c506ec773ef744cc2eacaee1f2
AmitZnati <amitznati@gmail.com>
2018-02-01 23:48:09 +0200

    db_install file

---
+++ db_install.cmd
@@ -0,0 +1,5 @@
+call php artisan migrate --path=database\migrations\User
+call php artisan migrate --path=database\migrations\POS
+call php artisan migrate --path=database\migrations\Admin
+call php artisan migrate
+call php artisan db:seed
